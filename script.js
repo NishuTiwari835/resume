cfc1fc27ef845c4ec7264e9f2777e1fedbb93c52
NishuTiwari835 <nishutiwarigo@gmail.com>
2024-12-23 14:25:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Coder'],
+    strings: ['Web Developer', 'Web Designer'],
     typeSpeed: 100,
     backspeed: 100,
     backDelay: 1000,
